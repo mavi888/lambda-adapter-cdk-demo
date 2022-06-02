1bcfa03391e8fca56c815b2440c0729637e55044
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-06-02 13:37:11 +0300

    Initial commit

---
+++ test/lambda-adapter-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaAdapterCdk from '../lib/lambda-adapter-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-adapter-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaAdapterCdk.LambdaAdapterCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
